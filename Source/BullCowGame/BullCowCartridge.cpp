6ee1fc218d4e1cefad97a6ae803b9ac0f5074e0c
Vince Zipparro <vincezipparro@gmail.com>
2020-05-26 23:33:23 -0500

    add pseudo code

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -7,22 +7,42 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
     PrintLine(TEXT("Guess the 4 letter word!")); 
     PrintLine(TEXT("Press enter to continue..."));
-    HiddenWord = TEXT("cake");
 
+    // setting up game
+    HiddenWord = TEXT("cake"); // set the hiddenword
+    // set lives
+
+    // prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
 
+    //checking playerguess
     if(Input == HiddenWord)
     {
         PrintLine(TEXT("Correct guess.."));
+
+
     }
     else
     {
         PrintLine(TEXT("Wrong guess.."));
     }
-    
-    // PrintLine(Input);
+
+    // check if isogram
+    // prompt to guess again
+    // check right number of characters
+    // prompt to guess again
+
+    // remove life
+
+    // check if lives > 0
+    // if yes guessagain
+    // show lives left
+    // if no show gameover and hiddenword
+    // prompt to playagain, press enter to play again?
+    // check user input
+    // playagain or quit
 }
