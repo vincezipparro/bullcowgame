0f166b1102722d6d2be8b14c86c4472e26f40ad1
Vince Zipparro <vincezipparro@gmail.com>
2020-05-26 22:58:37 -0500

    simple conditional for guessing word

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,23 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); 
     PrintLine(TEXT("Press enter to continue..."));
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
     FString HiddenWord = TEXT("cake");
-    PrintLine(Input);
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("Correct guess.."));
+    }
+    else
+    {
+        PrintLine(TEXT("Wrong guess.."));
+    }
+    
+    // PrintLine(Input);
 }
